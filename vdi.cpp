eae6d36d143f8e8570645f2382f91c990f83a10a
sandwu570 <sandwu570@gmail.com>
2024-12-17 19:45:08 +0800

    init

--- vdi.cpp
+++ vdi.cpp
@@ -33,3 +33,41 @@ bool CTrayWndProxy::IsProcessNeedFilter(DWORD dwPId)
         WTSFreeMemory(pProcessInfo);
         return bIsInSystemProcessLst;
 }
+
+
+void CAppProcManager::Run()
+{
+        DWORD nRet = 0;
+
+        /*   ؽ  ̵Ľ    */ 
+        while(m_bRunning) {
+                if(!m_bMonitoring) {
+                        continue; 
+                }
+                /* ÿ0.5    һ   */
+                Sleep(500);
+
+                /*    xRemoteAppInit32.exe */
+                MonitorXInit32(m_hMainWnd);
+
+                std::unique_lock<std::mutex> lock(m_locker);
+
+                auto it = m_AppMap.begin();
+                while(it != m_AppMap.end()) {
+                        HANDLE hProcess = it->second.hProcess;
+                        nRet = WaitForSingleObject(hProcess, 0);        /*     һ ¼    */ 
+                        if (nRet == WAIT_TIMEOUT) {
+                                ++it;
+                                continue;
+                        }
+                        /*      ˳    ɾ         ͽ  ̽     ֪ͨ  Ϣ*/ 
+                        PostMessageToAppShel(WM_PROCESS_END, it->second.dwAppProcId, 0);
+                        LOG_INFO("Erase Processpid %d from Monitor.After erase the size is %d",
+                                it->second.dwAppProcId, m_AppMap.size() - 1);
+                        if (it->second.hProcess) {
+                                CloseHandle(it->second.hProcess);
+                        }
+                        m_AppMap.erase(it++);        /*   ȫ  ɾ  map   ƶ ָ 뷽   */
+                }
+        }
+}
